46578e0b14bfb53da18e0b7a00a3fd1fe562ab94
Pavel Litvinenka <pasblek@gmail.com>
2021-12-24 13:07:53 +0300

    Comm1

---
+++ src/main/java/ru/intervale/DemoSpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package ru.intervale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringInitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/intervale/DemoSpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intervale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
